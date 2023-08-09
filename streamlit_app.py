b070f2770167b1f1960c369cde5595c9073b2229
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-09 11:53:41 +0530

    Update streamlit_app.py
    displays on selected fruits details on page

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,11 +13,18 @@ streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+#read csv file
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#Set index as Fruit name column
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
+
+#Display only selected fruits on the page.
+streamlit.dataframe(fruits_to_show)
