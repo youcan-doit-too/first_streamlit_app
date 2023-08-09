4c1d88e103a46845278ffa63529663573f8f9a91
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-09 11:54:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 #Set index as Fruit name column
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+# Let's put a pick list here so they can pick the fruit they want to include - default in square brackets
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
