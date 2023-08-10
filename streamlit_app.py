28968451be87cf603e69a27ff4d6cd171db5ac4f
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-10 21:58:51 +0530

    Update streamlit_app.py
    Tried changing index of fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized.set_index('genus'))
+streamlit.dataframe(fruityvice_normalized.set_index('name'))
