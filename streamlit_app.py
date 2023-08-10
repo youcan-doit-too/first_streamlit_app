27f6aaaf48106bc9c4218a6ff0ca45cd37982074
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-10 21:52:22 +0530

    Update streamlit_app.py
    added json as response

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,4 +32,4 @@ streamlit.dataframe(fruits_to_show)
 
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
