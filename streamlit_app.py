649d2dedce9627c4581f86378eeffae982a03ff3
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-10 21:50:11 +0530

    Update streamlit_app.py
    added request to get fruityvice response

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My Parents New healthy Diner!")
 
@@ -28,3 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Display only selected fruits on the page.
 streamlit.dataframe(fruits_to_show)
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
