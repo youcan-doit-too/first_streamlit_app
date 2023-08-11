f5322c439d311ff58250405d068ac17c0eb8c4ff
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-11 11:41:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,18 +34,21 @@ streamlit.dataframe(fruits_to_show)
 
 #New section for FruityVice
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-#streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-#streamlit.dataframe(fruityvice_normalized.set_index('name'))
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  #streamlit.write('The user entered ', fruit_choice)
+  
+  if not fruit_choice:
+       streamlit.error("Please select a fruit to get information.")
+  else
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+       #streamlit.text(fruityvice_response.json())
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       #streamlit.dataframe(fruityvice_normalized.set_index('name'))
+       streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
