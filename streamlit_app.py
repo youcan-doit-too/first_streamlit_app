6f6199af46cc0a208deeb8beeda1ad8745325765
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-11 11:42:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ try:
   
   if not fruit_choice:
        streamlit.error("Please select a fruit to get information.")
-  else
+  else:
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
        #streamlit.text(fruityvice_response.json())
        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
