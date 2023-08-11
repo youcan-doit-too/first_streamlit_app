98fa06b5bb27f4fb47d1a77bbdcb0a06310edcfa
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-11 11:45:50 +0530

    Update streamlit_app.py
    created function

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,11 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+def get_fruityvice_data(this_fruit_choice)
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       return streamlit.dataframe(fruityvice_normalized)
+
 streamlit.title("My Parents New healthy Diner!")
 
 streamlit.header("Breakfast Menu!")
@@ -41,11 +46,8 @@ try:
   if not fruit_choice:
        streamlit.error("Please select a fruit to get information.")
   else:
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-       #streamlit.text(fruityvice_response.json())
-       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       #streamlit.dataframe(fruityvice_normalized.set_index('name'))
-       streamlit.dataframe(fruityvice_normalized)
+       back_from_function = get_fruityvice_data(fruit_choice)
+       streamlit.dataframe(back_from_function)
     
 except URLError as e:
   streamlit.error()
