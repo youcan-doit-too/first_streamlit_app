7b3bb02f0d2cf759a3137fa17608c1799184f8b4
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-11 13:18:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ def get_fruit_load_list():
 
 def insert_row_snowflake(new_fruit):
        with my_cnx.cursor() as my_cur:
-              my_cur.execute("INSERT INTO FRUIT_LOAD_LIST values ('from streamlit') ")
+              my_cur.execute("INSERT INTO FRUIT_LOAD_LIST values ('" + :new_fruit "') ")
               return "Thanks for adding " + new_fruit
 
 #main code
@@ -67,6 +67,7 @@ except URLError as e:
 if streamlit.button('Get Fruit List Loaded'):
        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
        my_data_rows = get_fruit_load_list()
+       my_cnx.close()
        streamlit.dataframe(my_data_rows)
 
 #Let user add a fruit:
@@ -74,5 +75,6 @@ input_fruit = streamlit.text_input("What fruit would you like to add?")
 if streamlit.button('Add new fruit to list'):
        my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
        back_from_function = insert_row_snowflake(input_fruit)
+       my_cnx.close()
        streamlit.text(back_from_function)
 
