dcb97b7dc543eee9a771cd3310dbe0d765927ad0
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-11 11:47:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ from urllib.error import URLError
 def get_fruityvice_data(this_fruit_choice):
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-       return streamlit.dataframe(fruityvice_normalized)
+       return fruityvice_normalized
 
 streamlit.title("My Parents New healthy Diner!")
 
