39d54c288ab4a26a1f9c83ab9b0c31a63a950fef
youcan-doit-too <141806370+youcan-doit-too@users.noreply.github.com>
2023-08-11 11:03:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #streamlit.dataframe(fruityvice_normalized.set_index('name'))
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
